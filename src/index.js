103641fd6719bd7e0158a3c3f8f1734d9053a568
Scarlett Eller <rq6715@gmail.com>
2022-01-17 18:20:17 -0500

    commit tree df36dfa665ea3c0822cbd482fd9aea4fa11fb96f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const userInput = e.target.searchByID.value;
+      
+
+    fetch(`http://localhost:3000/movies/${userInput}`)
+        .then(res => {return res.json()})
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
